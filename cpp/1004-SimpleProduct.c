fb199d66ff284b66a9e788910fac5a3d1a85d361
Lucas Ribeiro <146796083+lucarueira@users.noreply.github.com>
2023-10-16 19:54:08 -0300

    beecrowd

---
+++ cpp/1004-SimpleProduct.c
@@ -0,0 +1,15 @@
+#include <iostream>
+int main(){
+    int a;
+    int b;
+    int prod;
+    
+    std::cin >> a;
+    std::cin >> b;
+    
+    prod = a*b;
+    
+    std::cout << "PROD = " << prod <<std::endl;
+    
+    
+}
