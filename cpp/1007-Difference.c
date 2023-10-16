c701da808779a29f404580597134c6464a5d8bae
Lucas Ribeiro <146796083+lucarueira@users.noreply.github.com>
2023-10-16 19:56:55 -0300

    beecrowd

---
+++ cpp/1007-Difference.c
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+    int a;
+    int b;
+    int c;
+    int d;
+    
+    std::cin >> a;
+    std::cin >> b;
+    std::cin >> c;
+    std::cin >> d;
+    
+    int dif;
+    
+    dif = (a*b)-(c*d);
+    
+    std::cout << "DIFERENCA = "<< dif <<std::endl;
+    
+    
+}
