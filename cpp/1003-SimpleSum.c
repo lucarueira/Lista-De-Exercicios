f67cd556b2af43b85a8c9c6c854ce4bb5d064ae5
Lucas Ribeiro <146796083+lucarueira@users.noreply.github.com>
2023-10-16 19:51:10 -0300

    beecrowd

---
+++ cpp/1003-SimpleSum.c
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int a;
+    int b;
+    int soma;
+    
+    std::cin >> a;
+    std::cin >> b;
+    
+    soma = a+b;
+    
+    std::cout << "SOMA = " << soma << std::endl;
+    
+}
