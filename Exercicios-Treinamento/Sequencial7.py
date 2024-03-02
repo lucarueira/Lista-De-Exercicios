c8277d4a73de3ef129a0052c78f2f17706b6425d
Lucas Ribeiro <lucasrarueira017@gmail.com>
2024-03-02 10:15:40 -0300

    aprendizado

---
+++ Exercicios-Treinamento/Sequencial7.py
@@ -0,0 +1,7 @@
+"""
+7. Faça um Programa que calcule a área de um quadrado, 
+em seguida mostre o dobro desta área para o usuário.
+"""
+lado = float(input("Digia o lado do quadrado : "))
+aread = (lado*lado)*2
+print("O dobro da área é : ",aread)
---
+++ Exercicios-Treinamento/Sequencial8.py
@@ -0,0 +1,9 @@
+"""
+8. Faça um Programa que pergunte quanto você ganha por hora e o número de horas
+trabalhadas no mês. 
+Calcule e mostre o total do seu salário no referido mês.
+"""
+phora = float(input("Quando você ganha por Hora ? "))
+horat = float(input("Quantas horas trabalhadas no mês ? "))
+salario = phora*horat
+print("Seu salario  é : R$ ", salario)
